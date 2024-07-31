6c70974f5f92b86c7a192521cd4267c66edac69d
Paul Odeon <mail@paulodeon.com>
2024-07-31 12:34:02 +0100

    Add Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_112216) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_31_113252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_112216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "aasm_state"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
